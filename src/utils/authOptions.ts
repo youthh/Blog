6263b90a937ad4bf7dbcb80666474122c70dd79b
youthh <valavskiylesha630@gmail.com>
2023-10-23 12:46:03 +0300

    initial

---
+++ src/utils/authOptions.ts
@@ -0,0 +1,29 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize(credentials: any, req) {
+        return {
+          id: "1",
+          Email: credentials.email,
+        };
+      },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
